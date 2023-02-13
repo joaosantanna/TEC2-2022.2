99e81ef1bbc35da3c11cf6d430f627baccb927f8
Joao Santanna <joaosantanna@yahoo.com.br>
2023-02-13 15:41:13 -0300

    atualizacao de 13/02

--- L1Q1.py
+++ L1Q1.py
@@ -4,12 +4,20 @@ numeros = []
 for i in range(10):
     numeros.append(randint(1,20))
 
+print(numeros)
+
 x = int(input('Informe o numero que vc procura:'))
 
 if x not in numeros:
     print(f'o numero {x} não está presente no vetor')
+
 else:
     for p,v in enumerate(numeros):
         if v == x:
             print(f'numero {x} está na posição {p}')
 print(numeros)
+
+
+
+
+
